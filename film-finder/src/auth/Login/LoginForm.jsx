67d5bb5203e588cc9d1e0e2e840f620e76d95b5d
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2024-08-24 20:55:24 +0700

    modifying page login
    modifying page login

--- film-finder/src/auth/Login/LoginForm.jsx
+++ film-finder/src/auth/Login/LoginForm.jsx
@@ -1,36 +1,36 @@
 import { useState } from 'react';
-import FormInput from '../../components/FormInput.jsx'; // Path ke FormInput.js
-import Button from '../../components/Button.jsx'; // Path ke Button.js
+import FormInput from '../../components/FormInput.jsx';
+import Button from '../../components/Button.jsx';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="space-y-6">
-      <FormInput
-        label="Email"
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <FormInput
-        label="Password"
-        type="password"
-        id="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button type="submit" text="Login" />
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit} className="space-y-6">
+            <FormInput
+                label="Username"
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <FormInput
+                label="Password"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" text="Login" />
+        </form>
+    );
 }
 
 export default LoginForm;
