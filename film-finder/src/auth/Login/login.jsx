3d3c1890ae5cc45f92ad14992c1333786fb09346
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2024-08-25 11:04:00 +0700

    modify login.jsx and modify index.css add gray bg colour

--- film-finder/src/auth/Login/login.jsx
+++ film-finder/src/auth/Login/login.jsx
@@ -2,7 +2,7 @@ import LoginForm from './LoginForm.jsx'; // Path ke LoginForm.js
 
 function Login () {
   return (
-    <div className="bg-custom-blue-light min-h-screen flex items-center justify-center bg-gray-100 ">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 ">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
         <h2 className="text-2xl font-bold text-center mb-8 text-custom-blue-dark">FilmFinder</h2>
         <LoginForm />
