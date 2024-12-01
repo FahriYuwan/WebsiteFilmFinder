a0bb39f28be5b5bd3cb72b4cea70f0bc1040dd1a
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2024-12-01 19:13:55 +0700

    penghapusan yg gak penting

--- resources/js/Components/ApplicationLogo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from '../assets/images/just-text.png'; // Adjust the path according to your folder structure
-
-export default function ApplicationLogo(props) {
-    return (
-        <img src={Logo} alt="Application Logo" {...props} />
-    );
-}
